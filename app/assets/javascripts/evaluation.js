7147ecd230f2e780eb9df403842d683b0e523ab5
meq <fewell@gmail.com>
2018-10-08 18:12:23 -0400

    Working on delete confirmation modal

--- app/assets/javascripts/evaluation.js
+++ app/assets/javascripts/evaluation.js
@@ -1,83 +1,162 @@
-function addEventHandler(){
+function addEventHandler() {
   $(".js-next").on("click", function(event) {
     let nextId = parseInt($(".js-next").attr("data-id"));
-    if (isNaN(nextId)){
-      $('.flash-messages').html(`
+    if (isNaN(nextId)) {
+      $(".flash-messages").html(`
         <div class="flash flash-warn">
           <p id="alert">End of Records</p>
         </div>
-        `)
+        `);
     } else {
-      $.get("/evaluations/" + nextId + ".json", function(evaluation){
-          $('.flash-messages').empty()
-          addEvalToDom(evaluation)
-          addEventHandler()
+      $.get("/evaluations/" + nextId + ".json", function(evaluation) {
+        $(".flash-messages").empty();
+        addEvalToDom(evaluation);
+        history.pushState(
+          { urlPath: "/evaluations/" + nextId },
+          "",
+          "/evaluations/" + nextId
+        );
+        addEventHandler();
       });
-      event.preventDefault()
+      event.preventDefault();
     }
-  })
+  });
   $(".js-prev").on("click", function(event) {
     let prevId = parseInt($(".js-prev").attr("data-id"));
-    if (isNaN(prevId)){
-      $('.flash-messages').html(`
+    if (isNaN(prevId)) {
+      $(".flash-messages").html(`
         <div class="flash flash-warn">
           <p id="alert">No Previous Record</p>
         </div>
-        `)
+        `);
     } else {
-      $.get("/evaluations/" + prevId + ".json", function(evaluation){                   $('.flash-messages').empty()
-        addEvalToDom(evaluation)
-        addEventHandler()
+      $.get("/evaluations/" + prevId + ".json", function(evaluation) {
+        $(".flash-messages").empty();
+        addEvalToDom(evaluation);
+        history.pushState(
+          { urlPath: "/evaluations/" + prevId },
+          "",
+          "/evaluations/" + prevId
+        );
+        addEventHandler();
       });
     }
-    event.preventDefault()
-  })
+    event.preventDefault();
+  });
 
-
-$(".js-copy").on("click", function(event) {
-  let copyId = parseInt($(".js-copy").attr("data-id"));
-  console.log(copyId)
-  if (isNaN(copyId)){
-    $('.flash-messages').html(`
+  $(".js-copy").on("click", function(event) {
+    let copyId = parseInt($(".js-copy").attr("data-id"));
+    console.log(copyId);
+    if (isNaN(copyId)) {
+      $(".flash-messages").html(`
       <div class="flash flash-warn">
         <p id="alert">It didn't copy!</p>
       </div>
-      `)
-  } else {
-    $.get("/evaluations/" + copyId + "/copy", function(evaluation){                   $('.flash-messages').empty()
-      window.location.href = "/evaluations/" + evaluation.id + "/edit" 
-    });
-  }
-  event.preventDefault()
-})
+      `);
+    } else {
+      $.get("/evaluations/" + copyId + "/copy", function(evaluation) {
+        $(".flash-messages").empty();
+        window.location.href = "/evaluations/" + evaluation.id + "/edit";
+      });
+    }
+    event.preventDefault();
+  });
 
+  // $(".js-delete").on("click", function(event) {
+  //   const deleteId = parseInt($(".js-delete").attr("data-id"));
+  //   if (isNaN(deleteId)) {
+  //     $(".flash-messages").html(`
+  //     <div class="flash flash-warn">
+  //       <p id="alert">It didn't get deleted</p>
+  //     </div>
+  //     `);
+  //   } else {
+  //     $.ajax({
+  //       url: "/evaluations/" + deleteId,
+  //       method: 'DELETE',
+  //     })
+  //       .done(function() {
+  //         $(".flash-messages").empty();
+  //       });
+  //   }
+  //   event.preventDefault();
+  // });
+
+  $(".ui.modal")
+    .modal({
+      closable: false,
+      onDeny: function() {
+        window.alert("Wait not yet!");
+        return false;
+      },
+      onApprove: function() {
+        window.alert("Approved!");
+      }
+    })
+    .modal("attach events", ".js-delete", "show");
 }
 
- function addEvalToDom(eval) {
-    eval.course = eval.course || {}
-    eval.next = eval.next || {}
-    eval.previous = eval.previous || {}
-    let newEval = new Evaluation(eval.id, eval.name, eval.course.title, eval.course.start_date, eval.questions, eval.next.id, eval.previous.id)
-     $('#evaluation-main').html(newEval.renderEval())
- }
+function addEvalToDom(eval) {
+  eval.course = eval.course || {};
+  eval.next = eval.next || {};
+  eval.previous = eval.previous || {};
+  let newEval = new Evaluation(
+    eval.id,
+    eval.name,
+    eval.course.title,
+    eval.course.start_date,
+    eval.questions,
+    eval.next.id,
+    eval.previous.id
+  );
+  $("#evaluation-main").html(newEval.renderEval());
+}
 
- function Evaluation(id, name, course, startDate, questions, next, previous, copy){
-   this.id = id
-   this.name = name
-   this.course = course
-   this.startDate = startDate
-   this.questions = questions
-   this.next = next
-   this.previous = previous
-   this.copy = copy
- }
+function Evaluation(
+  id,
+  name,
+  course,
+  startDate,
+  questions,
+  next,
+  previous,
+  copy
+) {
+  this.id = id;
+  this.name = name;
+  this.course = course;
+  this.startDate = startDate;
+  this.questions = questions;
+  this.next = next;
+  this.previous = previous;
+  this.copy = copy;
+}
 
- Evaluation.prototype.renderEval = function() {
-   let questionList = this.questions.reduce((questionString, question) => {
-     return questionString + `<li>${question.content}</li>`
-   }, "" )
- 
-   return `
+Evaluation.prototype.renderEval = function() {
+  let questionList = this.questions.reduce((questionString, question) => {
+    return questionString + `<li>${question.content}</li>`;
+  }, "");
+
+  return `
+  <div class="ui small basic test modal">
+    <div class="ui icon header">
+      <i class="archive icon"></i>
+      Archive Old Messages
+    </div>
+    <div class="content">
+      <p>Your inbox is getting full, would you like us to enable automatic archiving of old messages?</p>
+    </div>
+    <div class="actions">
+      <div class="ui red basic cancel inverted button">
+        <i class="remove icon"></i>
+        No
+      </div>
+      <div class="ui green ok inverted button">
+        <i class="checkmark icon"></i>
+        Yes
+      </div>
+    </div>
+  </div>
        <div class="d-inline-block mb-1">
          <h2>
             ${this.name}
@@ -99,7 +178,28 @@ $(".js-copy").on("click", function(event) {
        <div class="mt-3">
 
        </div>
-       <a href="#" class="js-prev btn" data-id="${this.previous}">Previous Evaluation</a>
-       <a href="#" class="js-next btn" data-id="${this.next}">Next Evaluation</a>
-       <a href="#" class="js-copy btn" data-id="${this.id}">Duplicate Evaluation</a>
-   `}
+       <div class="ui buttons">
+       <button class="ui left labeled icon button js-prev" data-id="${
+         this.previous
+       }">
+       <i class="left arrow icon"></i>
+       Previous
+       </button>
+
+       <button class="ui right labeled icon button js-next" data-id="${
+         this.next
+       }">
+           <i class="right arrow icon"></i>
+           Next
+           </button>
+    </div>
+    <button class="ui positive basic button js-copy" data-id="${
+      this.id
+    }">Duplicate</button>
+
+    <button class="ui negative basic button js-delete" data-id="${
+      this.id
+    }">Delete</button>
+       
+   `;
+};
