968389af9856d69502d217321f5dd9e33eeb2b2a
meq <fewell@gmail.com>
2018-10-09 18:03:51 -0400

    Adding Models to ActiveAdmin

---
+++ app/admin/courses.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Course do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+ permit_params :title, :location, :credits, :learning_objective_1, :learning_objective_2, :learning_objective_3, :start_date, :end_date, :published, :instructor_id, :evaluation_id 
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
---
+++ app/admin/evaluations.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register Evaluation do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+ permit_params :name, :course_id, :questions
+
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
